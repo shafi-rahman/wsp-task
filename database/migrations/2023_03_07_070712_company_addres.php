343a695087cb545193b4f9a8e23d0416e30f4ceb
shafi-rahman <s.rahman0365@gmail.com>
2023-03-07 12:39:02 +0530

    create company_address migration files and defile  schema

---
+++ database/migrations/2023_03_07_070712_company_addres.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_addres', function (Blueprint $table) {
+            $table->id();
+            $table->integer('client_info_id');
+            $table->string('company_address');
+            $table->string('company_city', 150);
+            $table->string('company_contrary', 150);
+            $table->string('company_email', 150);
+            $table->string('company_phone', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
